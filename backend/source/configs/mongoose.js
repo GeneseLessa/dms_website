91eacebc22b3bcbfc7d6579b010337644885dc07
Genese Lessa <genese.lessa@gmail.com>
2024-07-29 21:05:02 -0300

    removal of all last changes

--- backend/source/configs/mongoose.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const mongoURL = process.env.DB_CONNECTION;
-
-async function main() {
-  await mongoose.connect(mongoURL);
-}
-
-export default main;
--- backend/source/management/routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import firstUser from "./controllers/first-user.js";
-
-const router = Router();
-
-router.post("/first-user", (req, res) => firstUser.firstUser(req, res));
-
-export default router;
