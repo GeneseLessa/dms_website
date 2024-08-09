84ddc44c30da94a804599b49d3f3c7ac10ca764d
Genese Lessa <genese.lessa@gmail.com>
2024-08-08 23:17:32 -0300

    some pre-contents changes

---
+++ frontweb/src/applications/management/components/contents/Textual.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="textualContent">
+    <h1>Conteúdo textual</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TextualContent",
+  props: ["page"],
+  data() {
+    return {
+      content: "",
+      loading: false,
+      message: "",
+    };
+  },
+};
+</script>
--- frontweb/src/applications/management/components/pages/PageContents.vue
+++ frontweb/src/applications/management/components/pages/PageContents.vue
@@ -1,46 +1,68 @@
 <template>
   <div id="pageContents">
-    <h1>Componentes na página</h1>
-    <p>ID: {{ page }}</p>
+    <div v-if="step === 'index'" class="itemContents">
+      <h1>Componentes na página</h1>
+      <p>ID: {{ page }}</p>
 
-    <div class="contentsContainer">
-      <div class="contentsMenu">
-        <div class="content-box">
-          <h3>Textual</h3>
+      <div class="contentsContainer">
+        <div class="contentsMenu">
+          <div class="content-box" @click="step = 'textual'">
+            <h3>Textual</h3>
+          </div>
+          <div class="content-box">
+            <h3>Carrossel</h3>
+          </div>
+          <div class="content-box">
+            <h3>Álbum</h3>
+          </div>
+          <div class="content-box">
+            <h3>Formulário</h3>
+          </div>
+          <div class="content-box">
+            <h3>Lista</h3>
+          </div>
+          <div class="content-box">
+            <h3>Banner</h3>
+          </div>
+          <div class="content-box">
+            <h3>GMaps</h3>
+          </div>
         </div>
-        <div class="content-box">
-          <h3>Carrossel</h3>
-        </div>
-        <div class="content-box">
-          <h3>Álbum</h3>
-        </div>
-        <div class="content-box">
-          <h3>Formulário</h3>
-        </div>
-        <div class="content-box">
-          <h3>Lista</h3>
-        </div>
-        <div class="content-box">
-          <h3>Banner</h3>
-        </div>
-        <div class="content-box">
-          <h3>GMaps</h3>
+        <div class="contents">
+          <p>
+            Componentes são as pequenas partes que irão compôr as suas páginas.
+            Cada componente possui uma função distinta e, por ser modularizado,
+            o Manager poderá adicionar novos itens de página conforme sejam
+            desenvolvidos.
+          </p>
         </div>
       </div>
-      <div class="contents"></div>
+    </div>
+
+    <div v-if="step === 'textual'">
+      <TextualContent :page="page" />
     </div>
   </div>
 </template>
 
 <script>
+import TextualContent from "../contents/Textual.vue";
+
 export default {
   name: "PageContents",
   props: ["page"],
+  components: { TextualContent },
+  data() {
+    return {
+      step: "index",
+    };
+  },
 };
 </script>
 
 <style lang="css" scoped>
-#pageContents {
+#pageContents,
+.itemContents {
   display: flex;
   flex-direction: column;
   gap: 10px;
